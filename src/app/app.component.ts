30049bc87a101865c1978c4daa76e680c374ea93
Vanderson Noleto <81515355+vannoleto@users.noreply.github.com>
2025-03-21 16:18:19 -0300

    components createds with styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { HomeComponent} from './pages/home/home.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuBarComponent, MenuTitleComponent, HomeComponent, BigCardComponent, SmallCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
